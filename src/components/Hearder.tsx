c3ea89f93ab6c2fa4d068342e0067a8b5ab88d49
kwonkangbin <kbk282655@gmail.com>
2023-02-16 03:42:37 +0900

    CREATE : 파일삭제 및 헤더폴더 생성

---
+++ src/components/Hearder.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';//npm i @types/styled-components       
+
+export default function Hearder() {
+  return (
+    <header>
+        <div>
+            
+        </div>
+    </header>
+  );
+}
+
+//git config --global core.autocrlf true
