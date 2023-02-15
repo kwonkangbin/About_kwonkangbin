fad8b1e8d82128c0e081305dcb3f1b13572c6a02
kwonkangbin <kbk282655@gmail.com>
2023-02-16 03:01:59 +0900

    타입스크립트로 다시 만듬

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return (  
-    <div>
-      <header>
-        <ul>
-          <li>A</li>
-          <li>B</li>
-          <li>C</li>
-        </ul>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
 
 export default App;
-
-//git config --global core.autocrlf true
