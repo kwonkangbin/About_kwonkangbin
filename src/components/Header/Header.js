d8bdc38010a58aa104a2d09a18a717914a6758cc
kwonkangbin <kbk282655@gmail.com>
2023-02-19 22:15:33 +0900

    revert "UPDATE : 헤더바 수정"
    This reverts commit cc9c5753b82840b205261a26735aa9f7f86eac71.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
 import * as S from './style.js';
 
 export default function Header() {
-    const [ref, inView] = useInView({ initialInView: true });
+    //const [ref, inView] = useInView({ initialInView: true });
     return ( 
         <>
-            <div ref={ref} />
+            {/* <S.dif ref={ref} /> */}
             <S.Styledheader className={!inView ? "scrolled" : ""}>
                 <S.StyledLink to='/'>kangbin</S.StyledLink>
                 <S.StyledA href="https://github.com/kwonkangbin">github</S.StyledA>
@@ -16,3 +15,5 @@ export default function Header() {
         </> 
     );
 }
+
+
