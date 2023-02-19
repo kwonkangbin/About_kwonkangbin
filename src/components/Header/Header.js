cc9c5753b82840b205261a26735aa9f7f86eac71
kwonkangbin <kbk282655@gmail.com>
2023-02-19 22:14:11 +0900

    Revert "72c6a9ba1417f3b63742fa2c5e08751622c555aa헤더바"
    This reverts commit 73c6a9ba1417f3b63742fa2c5e08751622c555aa.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
 import * as S from './style.js';
 
 export default function Header() {
-    //const [ref, inView] = useInView({ initialInView: true });
+    const [ref, inView] = useInView({ initialInView: true });
     return ( 
         <>
-            {/* <S.dif ref={ref} /> */}
+            <div ref={ref} />
             <S.Styledheader className={!inView ? "scrolled" : ""}>
                 <S.StyledLink to='/'>kangbin</S.StyledLink>
                 <S.StyledA href="https://github.com/kwonkangbin">github</S.StyledA>
@@ -15,5 +16,3 @@ export default function Header() {
         </> 
     );
 }
-
-
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -4,21 +4,16 @@ import { Link } from "react-router-dom";
 export const Styledheader = styled.header`
     position: fixed;
     top : 10px;
-    z-index: 99999999;
     display: flex;
     align-items: center;
     height: 70px;
     width: 100%;
     max-width: 900px;
     border-radius: 12px;
-    background-color: rgba(255, 255, 255, 0);
-    backdrop-filter: blur(8px);
-    transition: background-color 0.2s, box-shadow 0.2s;
-
 
     &.scrolled {
-        background-color: rgba(255, 255, 255, 0.7);
-        box-shadow: 0 0 10px rgba(230, 230, 230, 1);    
+        background-color: rgba(36, 36, 36, 0.7);
+        box-shadow: 0 0 10px rgba(230, 230, 230, 1);
     }
 `;
 
@@ -34,9 +29,4 @@ export const StyledA = styled.a`
     color: black;
     font-size: 30px;
     font-weight: 500;
-`;
-
-export const dif = styled.div`
-    height: 10px;
-    width: 100%;
 `;
