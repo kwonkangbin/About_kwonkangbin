ca07d22cda5a7d53f3122cc83de28a6df1f91902
kwonkangbin <kbk282655@gmail.com>
2023-02-19 23:24:44 +0900

    CREATE : Content

---
+++ src/components/Content/Content.js
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from './style.js';
+
+export default function Content() {
+    return (
+        <S.Stylecontent>
+            <h1>
+                안녕하세요
+                <br></br>
+                유사개발자
+                <br></br>
+                권강빈입니다.
+            </h1>
+        </S.Stylecontent>
+    );
+}
---
+++ src/components/Content/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
+export const Stylecontent = styled.main`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-top: 70px;
+    >h1{
+        font-size: 0.7em;
+        font-weight: bold;
+        text-align: start;
+        line-height: 180%;
+    }
+`;
