2f97d3b02b5b41821e8231c2635ee818623e3068
kwonkangbin <kbk282655@gmail.com>
2023-02-24 01:51:30 +0900

    UPDATE

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Education from "./Education.js";
 import Introduction from "./Introduction.js";
+import Awards from "./Awards.js";
+import Experiences from "./Experiences.js";
 import * as S from './style.js';
 
 export default function Content() {
     return (
         <S.Stylecontent>
             <Introduction />
+            <Experiences />
+            <Awards />
             <Education />
         </S.Stylecontent>
     );
