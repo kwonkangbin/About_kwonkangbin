fc9bf53b8b010b71c4428a4c8061d342515ad426
kwonkangbin <kbk282655@gmail.com>
2023-02-24 19:57:38 +0900

    CREATE : Project.js

--- src/components/Content/Project.js
+++ src/components/Content/Project.js
@@ -8,7 +8,9 @@ export default function Project(){
         <S.FadeIn>
             <h2>프로젝트</h2>
             <h3>Ressum</h3>
-            <p>자녀 맞춤형 독서 교육 서비스</p>
+            <p>간단 소개 : 자녀 맞춤형 독서 교육 서비스</p>
+            <p>사용 기술 : React, SCSS, firebase, Figma(UI/UX)</p>
+            <p>역할 : 프론트엔드 개발 및 기획</p>
             <LinkButton to="https://github.com/Ressum/FrontEnd">자세히</LinkButton>
         </S.FadeIn>
     );
