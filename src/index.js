16574b19a8774cb026e33379733dbd40a4e324c3
kwonkangbin <kbk282655@gmail.com>
2023-03-02 13:32:39 +0900

    힘들다.......

--- src/index.js
+++ src/index.js
@@ -22,10 +22,10 @@ const GlobalStyle = createGlobalStyle`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <>
         <GlobalStyle />
         <RouterProvider router={router} />
-    </React.StrictMode>
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
