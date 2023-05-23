3cb9341c44c9afc4c70f4baaabc7f1a018efa860
kwonkangbin <kbk282655@gmail.com>
2023-05-23 18:02:04 +0900

    background-color 적용

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import router from './Router';
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    body {
+  body {
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    background-color: white;
   }
 
   code {
